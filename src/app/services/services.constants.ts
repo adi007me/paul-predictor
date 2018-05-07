a1f490225761b27a9a538ee489b381b341863723
Aditya Bhave <adi007me@gmail.com>
2018-05-07 18:42:44 -0400

    Updated URL

--- src/app/services/services.constants.ts
+++ src/app/services/services.constants.ts
@@ -3,11 +3,9 @@ import { environment } from "../../environments/environment";
 export class Constants {
     public static get BASE_URL(): string {
         if (environment.production) {
-            return 'https://paul-webapi.cfapps.io/';
+            return 'https://paul-api.cfapps.io/';
         } else {
             return 'http://localhost:1337/';
         }
     }
-    //https://paul-webapi.cfapps.io/
-    //http://localhost:1337/
 }
