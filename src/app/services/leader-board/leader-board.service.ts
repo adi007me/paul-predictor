57958f014ff54b6eb77c384a1d23cf8c98064a2d
adi007me <adi007me@gmail.com>
2020-03-03 14:51:01 +0530

    Upgrade (#12)
    * Upgrade WIP
    
    * WIP Upgrade
    
    * Upgraded to Angular 9
    
    * Upgraded to Angular 9

--- src/app/services/leader-board/leader-board.service.ts
+++ src/app/services/leader-board/leader-board.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { LeaderBoard } from './leader-board';
 import { HttpClient } from '@angular/common/http';
 import { Constants } from '../services.constants';
