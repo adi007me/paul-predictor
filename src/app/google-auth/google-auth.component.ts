e44291dcb5053444d4b04441b79b900846fbb8d7
adi007me <adi007me@gmail.com>
2020-03-04 07:34:32 +0000

    Google Sign In

--- src/app/google-auth/google-auth.component.ts
+++ src/app/google-auth/google-auth.component.ts
@@ -14,6 +14,7 @@ export class GoogleAuthComponent implements OnInit, AfterViewInit {
   constructor(private changeDetector: ChangeDetectorRef, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+
   }
 
   public auth2: any;
@@ -23,13 +24,21 @@ export class GoogleAuthComponent implements OnInit, AfterViewInit {
 
   public googleInit() {
     gapi.load('auth2', () => {
-      this.auth2 = gapi.auth2.init({
+      gapi.auth2.init({
         client_id: '404042679583-o3ndihu0l83lje6q68408norqtp6ul6q.apps.googleusercontent.com',
         cookiepolicy: 'single_host_origin',
         scope: 'profile email'
-      });
+      }).then((auth) => {
+        console.log(auth.isSignedIn.get())
 
-      this.attachSignin(document.getElementById('googleSignIn'));
+        if (auth.isSignedIn && auth.isSignedIn.get()) {
+          this.loggedInSuccess.call(this, auth.currentUser.get());
+        }
+
+        this.auth2 = auth;
+
+        this.attachSignin(document.getElementById('googleSignIn'));
+      });
     });
   }
 
@@ -37,35 +46,37 @@ export class GoogleAuthComponent implements OnInit, AfterViewInit {
     const component = this;
 
     this.auth2.attachClickHandler(element, {},
-      function (googleUser) {
-
-        let profile = googleUser.getBasicProfile();
-
-        //TODO : send token to BE service
-        console.log('Token || ' + googleUser.getAuthResponse().id_token);
-
-        console.log('ID: ' + profile.getId());
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-
-        let profileInfo = {
-          id: profile.getId(),
-          name: profile.getName(),
-          imageUrl: profile.getImageUrl(),
-          email: profile.getEmail()
-        };
-
-        component.profileInfo = profileInfo;
-
-        component.loggedIn = true;
-        component.profileImage = profile.getImageUrl();
-
-        component.changeDetector.detectChanges();
-      }, function (error) {
+      this.loggedInSuccess.bind(this), function (error) {
         // TODO Show error in dialog
         alert(JSON.stringify(error, undefined, 2));
       });
+  }
+
+  loggedInSuccess(googleUser) {
+      console.log('inside loggedInSuccess', googleUser);
+      let profile = googleUser.getBasicProfile();
+
+      //TODO : send token to BE service
+      console.log('Token || ' + googleUser.getAuthResponse().id_token);
+
+      console.log('ID: ' + profile.getId());
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+
+      let profileInfo = {
+        id: profile.getId(),
+        name: profile.getName(),
+        imageUrl: profile.getImageUrl(),
+        email: profile.getEmail()
+      };
+
+      this.profileInfo = profileInfo;
+
+      this.loggedIn = true;
+      this.profileImage = profile.getImageUrl();
+
+      this.changeDetector.detectChanges();
   }
 
   accountInfoPopover() {
@@ -74,7 +85,21 @@ export class GoogleAuthComponent implements OnInit, AfterViewInit {
     });
   }
 
-  ngAfterViewInit(){
+  signOut() {
+    let component = this;
+
+    this.auth2.signOut().then(function () {
+      console.log('User signed out.');
+
+      component.profileInfo = {};
+
+      component.loggedIn = false;
+
+      component.changeDetector.detectChanges();
+    });
+  }
+
+  ngAfterViewInit() {
     this.googleInit();
   }
 }
