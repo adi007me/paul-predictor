87d4aeee3be4eab44e8ab2878b528914083db800
Aditya Bhave <adi007me@gmail.com>
2020-09-10 00:06:54 +0530

    Fixed Api URL

--- src/app/services/services.constants.ts
+++ src/app/services/services.constants.ts
@@ -3,7 +3,7 @@ import { environment } from "../../environments/environment";
 export class Constants {
     public static get BASE_URL(): string {
         if (environment.production) {
-            return window.location.host.split('.')[0] + '-api';
+            return `https://${window.location.host.split('.')[0] + '-api'}`;
         } else {
             return 'http://localhost:3000';
         }
